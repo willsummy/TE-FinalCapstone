99f711601da5ba11e9146e38eb73196d73c7890c
Wes Woods <weswoodscoding@gmail.com>
2021-08-04 10:39:13 -0400

    home and report pothole stylized 1

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
+<div id="master">
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <!-- <p>You must be authenticated to see this</p> -->
     <router-link v-bind:to="{name: 'report-form'}" >Report A Pothole</router-link>
   </div>
+</div>
 </template>
 
 <script>
@@ -11,3 +13,26 @@ export default {
   name: "home",
 };
 </script>
+
+
+<style>
+#master {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: space-around;
+    margin: auto;
+    border-style: groove;
+    border-color: black;
+    padding-left: 150px;
+    padding-right: 150px;
+    padding-bottom: 50px;
+    
+  }
+
+</style>
