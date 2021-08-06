e3fc6c2fc8d3448f7ba284c945ce75219a2b008b
William Summy <willsummy814@gmail.com>
2021-08-06 13:37:58 -0400

    Pushing for Wes, something went wonky

--- vue/src/views/PotholeDetails.vue
+++ vue/src/views/PotholeDetails.vue
@@ -6,6 +6,18 @@
       </div>
       <div id="map">
         <!-- map and description -->
+        <GmapMap
+      :center='center'
+      :zoom='10'
+      style='width:100%;  height: 400px;'
+      >
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in filteredMarkers"
+          :position="m.position"
+          @click="center=m.position"
+        />
+      </GmapMap>
       </div>
     </div>
     <div id="details-buttons">
