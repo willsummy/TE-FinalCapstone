63c4f1324cb03152afc674c101b42c6a876c9c64
William Summy <willsummy814@gmail.com>
2021-08-06 13:28:54 -0400

    PotholeDetails basic DOM structure

--- vue/src/views/PotholeDetails.vue
+++ vue/src/views/PotholeDetails.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>card details</div>
+  <div id="details-main">
+    <div id="details-map">
+      <div id="details">
+        <!-- everything but description -->
+      </div>
+      <div id="map">
+        <!-- map and description -->
+      </div>
+    </div>
+    <div id="details-buttons">
+      <!-- 3 buttons, edit, delete, status -->
+    </div>
+  </div>
 </template>
 
 <script>
