dc3e7fa0d13d6060f11a9030b7728aaf820c85c0
Wes Woods <weswoodscoding@gmail.com>
2021-08-09 11:46:57 -0400

    created servicecard and servicelist

--- vue/src/components/ServiceCard.vue
+++ vue/src/components/ServiceCard.vue
@@ -1,9 +1,46 @@
 <template>
+<div>
+    <div>
+        <table >
+          <tr>
+            <th>Service Id</th>
+            <td>{{service.service_id}}</td>
+          </tr>
+          <tr>
+            <th>Pothole Id</th>
+            <td>{{service.pothole_id}}</td>
+          </tr>
+          <tr>
+            <th>Date Reported</th>
+            <td>{{service.date_reported}}</td>
+          </tr>
+          <tr>
+            <th>Date Inspected</th>
+            <td>{{service.date_inspected}}</td>
+          </tr>
+          <tr>
+            <th>Employee Id</th>
+            <td>{{service.employee_id}}</td>
+          </tr>
+          <tr>
+            <th>Service Status Id</th>
+            <td>{{service.service_status_id}}</td>
+          </tr>
+        </table>
+
+    </div>
+
+
+
+</div>
 
 </template>
 
 <script>
+
 export default {
+    name: 'service-card',
+    props: ['service']
 
 }
 </script>
--- vue/src/components/ServiceList.vue
+++ vue/src/components/ServiceList.vue
@@ -1,9 +1,20 @@
 <template>
-
+<div>
+    <service-card id="service-list" v-for="service in services" v-bind:key="service.service_id" v-bind:service="service" ></service-card>
+</div>
 </template>
 
 <script>
+
+import ServiceCard from '../components/ServiceCard.vue';
+
 export default {
+    name: 'service-list',
+    props: ['services'],
+    components: {
+        ServiceCard
+
+    },
 
 }
 </script>
